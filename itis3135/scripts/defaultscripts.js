1fe405a8ea9420b751d7b6a60b4c6dbe2d87ede0
braxhaight47 <bhaight@uncc.edu>
2022-09-26 18:52:46 -0400

    j

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -1,9 +1,12 @@
+
 let input = prompt("What is your name?");
 document.getElementById("greeting").innerHTML = ("Welcome " + input + "!");
+
 function scriptTest() {
   // code to be executed
   alert("Hey my script is running");
 }
+
 function displayTime() {
     var today = new Date();
     var currentTime = today.toLocaleTimeString();
