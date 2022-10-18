ecfcca771d909b128b2497c2cff35342b1006587
braxhaight47 <bhaight@uncc.edu>
2022-10-18 17:59:51 -0400

    h

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -18,8 +18,8 @@ function getTodaysDate() {
     alert(today);
 }
 
-function promptUser() {
-    let name = prompt("What is your name?");
-    let mood = prompt("What is your mood?");
+function promptUser(name, mood) {
+    let name = name;
+    let mood = mood;
     document.getElementById("moodGreeting").innerHTML = ("The Blissful Hippo Welcomes you, " + name + "! We're glad you are doing " + mood);
 }
