fd49be98ac9e0c0417628fe2574c9581a0bd249d
braxhaight47 <bhaight@uncc.edu>
2022-11-03 16:28:44 -0400

    s

---
+++ itis3135/client/scripts/functions.js
@@ -0,0 +1,12 @@
+let pageName = "";
+function show(shown) {
+  pageNameArray = document.getElementsByClassName("target");
+  for (let i = 0; i < pageNameArray.length; i++) {
+      if(pageNameArray[i].id == shown){
+           pageNameArray[i].style.display = "block";
+      }
+      else {
+          pageNameArray[i].style.display = "none";
+      }
+    }
+}
