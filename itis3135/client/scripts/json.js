2a1b1837ba49db5651bd3b7334c43a08337ee8d6
braxhaight47 <bhaight@uncc.edu>
2022-12-05 18:26:48 -0500

    s

--- itis3135/client/scripts/json.js
+++ itis3135/client/scripts/json.js
@@ -4,7 +4,7 @@ $.each(data, function() {
 $.each(this, function(key, value) {
 $("#faculty").append(
 "<img src="+"'"+value.image+"'><br>"+
-"<h2>" + value.full_name + "</h2>" +
+"<h2>" + value.full_name + "</h3>" +
 "<h3>" + value.department + "</h3>" +
 "<p>" + value.bio + "</p><br>"
 );
