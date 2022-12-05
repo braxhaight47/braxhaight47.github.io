4b69525da8aa6835dc989cc517641ca86b67512a
braxhaight47 <bhaight@uncc.edu>
2022-12-05 18:08:58 -0500

    s

--- itis3135/scripts/Activity12.js
+++ itis3135/scripts/Activity12.js
@@ -1,9 +1,25 @@
 $(document).ready(function() {
-    $("#slider").bxSlider({
-        auto: true,
-        minSlides: 2,
-        maxSlides: 2,
-        slideWidth: 300,
-        slideMargin: 20
-    });
+
+$("#slider").bxSlider({
+
+auto: true,
+
+minSlides: 1, // Changed from 2 to 1 for Pt.3,4. Enable carousel moves one slide and shows one image at a time.
+
+maxSlides: 1, // Changed from 2 to 1 for Pt.3,4. Enable carousel moves one slide and shows at a time.
+
+// slideWidth: 250, // Removed for Pt. 4 , shows one image at a time on full div.
+
+slideMargin: 10,
+
+shrinkItems: true, // For Pt.4 shows one image on full div.
+
+randomStart: true, // Changed for Point 2 - Images to be loaded randomly
+
+pause: 3000, // Point 5, - For Automatic transition to 3 seconds i.e. 3000 mili seconds.
+
+captions:true, // Point 7 - Enable title as captions on each image.
+
+});
+
 });
