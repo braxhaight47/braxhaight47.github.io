f89d587c13f2429b6f3873ae3f5580edc87a3a20
braxhaight47 <bhaight@uncc.edu>
2022-12-05 18:05:42 -0500

    s

---
+++ itis3135/scripts/Activity12.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("#slider").bxSlider({
+        auto: true,
+        minSlides: 2,
+        maxSlides: 2,
+        slideWidth: 300,
+        slideMargin: 20
+    });
+});
