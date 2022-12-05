8ae79a079fb344abf0658aa3996a61262db0e668
braxhaight47 <bhaight@uncc.edu>
2022-12-05 18:30:17 -0500

    s

--- itis3135/client/scripts/json.js
+++ itis3135/client/scripts/json.js
@@ -2,11 +2,8 @@ $(document).ready(function(){
 $.getJSON("facultyList.json", function(data) {
 $.each(data, function() {
 $.each(this, function(key, value) {
-$("#faculty").append(
-"<img src="+"'"+value.image+"'><br>"+
-"<h2>" + value.full_name + "</h3>" +
-"<h3>" + value.department + "</h3>" +
-"<p>" + value.bio + "</p><br>"
+$("#contact").append(
+"<p>" + value.email + "</p>" +
 );
 });
 });
