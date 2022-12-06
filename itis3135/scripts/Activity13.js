29ed73a6bd6702208e6efa2739487babe9c71acb
braxhaight47 <bhaight@uncc.edu>
2022-12-05 19:48:25 -0500

    s

--- itis3135/scripts/Activity13.js
+++ itis3135/scripts/Activity13.js
@@ -3,7 +3,6 @@ $.getJSON("team.json", function(data) {
 $.each(data, function() {
 $.each(this, function(key, value) {
 $("#team").append(
-"<img src="+"'"+value.image+"'><br>"+
 "<h2>" + value.name + "</h2>" +
 "<h3>" + value.title + "</h3>" +
 "<p>" + value.bio + "</p><br>"
