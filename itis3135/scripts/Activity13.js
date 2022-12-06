c0cc4a3117bbf3b899613802bb664527965ab076
braxhaight47 <bhaight@uncc.edu>
2022-12-05 19:47:07 -0500

    s

--- itis3135/scripts/Activity13.js
+++ itis3135/scripts/Activity13.js
@@ -4,8 +4,8 @@ $.each(data, function() {
 $.each(this, function(key, value) {
 $("#team").append(
 "<img src="+"'"+value.image+"'><br>"+
-"<h2>" + value.full_name + "</h2>" +
-"<h3>" + value.department + "</h3>" +
+"<h2>" + value.name + "</h2>" +
+"<h3>" + value.title + "</h3>" +
 "<p>" + value.bio + "</p><br>"
 );
 });
