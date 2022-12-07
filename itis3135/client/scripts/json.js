9e1db6034d2734ed821238821b18ea82a1cb67ca
braxhaight47 <bhaight@uncc.edu>
2022-12-07 15:49:53 -0500

    s

--- itis3135/client/scripts/json.js
+++ itis3135/client/scripts/json.js
@@ -3,10 +3,9 @@ $.getJSON("contact.json", function(data) {
 $.each(data, function() {
 $.each(this, function(key, value) {
 $("#contact").append(
-"<p class='backColor2'>Contact Information:<br>" +
-"If you need additional help contact:<br>" +
-value.email + "<br>" +
-"If you need help regarding this website:<br>" +
+"<p class='backColor2'>Owners Email<br>" +
+value.email + "<br><br>" +
+"Central Website Help Email<br>" +
 value.alt_email + "</p>"
 );
 });
