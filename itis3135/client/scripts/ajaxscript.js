be17f5c3edd5ea6ffd590ec89905e2d2ca4318e6
braxhaight47 <bhaight@uncc.edu>
2022-12-07 15:21:27 -0500

    s

--- itis3135/client/scripts/ajaxscript.js
+++ itis3135/client/scripts/ajaxscript.js
@@ -1,5 +1,21 @@
-$(document).ready(function(){
-  $("button").click(function(){
-    $("#moreContent").load("ajaxx.txt");
-  });
-});
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("contact2").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "ajaxx.txt", true);
+  xhttp.send();
+}
+
+function loadDoc2() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("contact3").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "ajaxx2.txt", true);
+  xhttp.send();
+}
