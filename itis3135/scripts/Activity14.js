b814559176adeb715ffbd6f41d8b8fd1f7ef80a3
braxhaight47 <bhaight@uncc.edu>
2022-12-07 12:15:05 -0500

    s

--- itis3135/scripts/Activity14.js
+++ itis3135/scripts/Activity14.js
@@ -1,3 +1,25 @@
-$(document).ready(function() {
-	
+$(document).ready(function () {
+//on click for <a> element
+$("#nav_list li").click(function() {
+var title = $(this).children("a").attr("title");
+var filename = "json_files/"+title+".json";
+consumeJSON(filename);
+});
+
 }); // end ready
+function consumeJSON(jsonFileURL) {
+$.ajax({
+url: jsonFileURL,
+//handle as text
+dataType: "text",
+success: function (data) {
+//data downloaded + pass data
+var json = $.parseJSON(data);
+// display results
+$("main > h2").html(json.speakers[0].month + "<br/>" + json.speakers[0].speaker);
+$("main > h1").html(json.speakers[0].title);
+$("main > img").attr("src", json.speakers[0].image);
+$("main > p").html(json.speakers[0].text);
+}
+});
+}
